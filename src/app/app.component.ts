06a341af09828230be1dc6085687eca0e985ec04
varun-kumar06 <kumarmvarun39@gmail.com>
2024-10-05 16:37:46 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Time } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pipeproject';
+
+  price = 2000;
+
+  mydate : Date=new Date();
+  
+}
